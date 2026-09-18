6a89b258d8e940b18e6e1c054ddd523d171e831d
Tengen Ruise <tengenrusei@gmail.com>
2026-09-18 14:54:24 +0200

    Finalise Project

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    music.stopAllSounds()
+})
 basic.showString("BloodLens")
 basic.forever(function () {
     if (PlanetX_Basic.waterLevel(PlanetX_Basic.AnalogRJPin.J1) < 30) {
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
         PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J2, false)
-        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.LoopingInBackground)
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
     } else {
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, false)
         PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J2, true)
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, false)
     }
 })
